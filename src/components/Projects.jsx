bf4308d0fa31e707c25203e3fbff6a8b153e43d6
abc <abc@androidname.local>
2025-08-12 20:02:14 +0530

    changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,14 +13,14 @@ const Projects = () => {
         <div className="project-card">
           <img src={weatherAppImage} alt="Weather Web App" />
           <h3>Weather Web App</h3>
-          <a href="https://projectweatherappweb.netlify.app" target='_blank'>click here</a>
+          <a href="https://projectweatherappweb.netlify.app" target='_blank' rel="noopener noreferrer">click here</a>
           <p>Uses a weather API to fetch current weather details. Built with JavaScript, HTML, CSS.</p>
         </div>
 
         <div className="project-card">
           <img src={cryptoDashboardImage} alt="Crypto Dashboard" />
           <h3>Crypto Dashboard</h3>
-          <a href="https://crypto-tarcker-web-app.netlify.app" target='_blank'>click here</a>
+          <a href="https://crypto-tarcker-web-app.netlify.app" target='_blank' rel="noopener noreferrer">click here</a>
           <p>Built with React and API integration to display real-time cryptocurrency prices and trends.</p>
         </div>
 
